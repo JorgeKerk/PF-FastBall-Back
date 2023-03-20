bada32db2f16193797973ba6c624225c28254d3f
Jorge Kerk <jorgekerk@gmail.com>
2023-03-20 07:18:59 -0300

    deploy

--- src/bd/db.js
+++ src/bd/db.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, DB_DEPLOY } = process.env;
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 
 //DB_NAME is the database name
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+//   {
+//     logging: false,
+//     native: false,
+//   }
+// );
+//DB_DEPLOY is the database deployed
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  DB_DEPLOY,
   {
     logging: false,
     native: false,
